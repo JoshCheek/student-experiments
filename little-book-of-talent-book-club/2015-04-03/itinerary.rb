9e4ffa1e570b915586bd094549f33d31f2ca7f25
Josh Cheek <josh.cheek@gmail.com>
2015-04-11 09:36:42 -0600

    Begin implementing the DSL.
    Mostly turned into a talk about the object model,
    but I think it was a good talk, so it was worht it :)

--- little-book-of-talent-book-club/2015-04-03/itinerary.rb
+++ little-book-of-talent-book-club/2015-04-03/itinerary.rb
@@ -1,5 +1,27 @@
-# ruby itinerary.rb meeting1.rb
+class Itinerary
+  class Dsl
+    def self.parse(raw_itinerary)
+      Dsl.new.eval(raw_itinerary)
+    end
+
+    def eval(raw_itinerary)
+      super raw_itinerary
+    end
+
+    def name(name)
+      @name = name
+    end
+  end
+
+  module ToHtml
+    def self.call(itinerary)
+      itinerary
+    end
+  end
+end
+
+# $ ruby itinerary.rb meeting1.rb
 filename      = ARGV.first
 raw_itinerary = File.read(filename)
-itinerary     = Itinerary::Dsl.new(raw_itinerary).parse
-puts Itinerary::ToHtml.new(itinerary).call
+itinerary     = Itinerary::Dsl.parse(raw_itinerary)
+puts Itinerary::ToHtml.call(itinerary)
